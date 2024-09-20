dfcfe3f813136620dbb3427eac7c4d1e4611cf18
linhdat0709 <datnguyen2000nmnd@gmail.com>
2024-09-20 15:21:31 +0700

    fix(no jira): fix eslint prettier

--- components/awesome/Welcome.vue
+++ components/awesome/Welcome.vue
@@ -59,16 +59,10 @@ onMounted(() => {
     <LayoutPageSection class="flex-1 flex">
       <div class="flex-1 flex flex-col items-center justify-center">
         <h1 class="text-center mt-4">
-          <span
-            v-for="(item, i) in leadingsText"
-            :key="i"
-            :style="`--content: '${item.text}'; --start-color: ${
-              item.startColor
-            }; --end-color: ${item.endColor}; --animation-name: anim-fg-${
-              i + 1
+          <span v-for="(item, i) in leadingsText" :key="i" :style="`--content: '${item.text}'; --start-color: ${item.startColor
+            }; --end-color: ${item.endColor}; --animation-name: anim-fg-${i + 1
             }`"
-            class="animated-text-bg drop-shadow-xl text-6xl sm:text-8xl md:text-8xl lg:text-8xl 2xl:text-8xl block font-black uppercase"
-          >
+            class="animated-text-bg drop-shadow-xl text-6xl sm:text-8xl md:text-8xl lg:text-8xl 2xl:text-8xl block font-black uppercase">
             <span class="animated-text-fg">{{ item.text }}</span>
           </span>
         </h1>
@@ -78,64 +72,39 @@ onMounted(() => {
             'a starter template for Nuxt 3 with minimalist themes design, built in components, drawer & menus, and more.'
           }}
         </div>
-        <div
-          v-if="showAlert"
-          class="mt-4 w-auto text-center text-white bg-gray-800 rounded px-4 py-1 text-sm"
-        >
+        <div v-if="showAlert" class="mt-4 w-auto text-center text-white bg-gray-800 rounded px-4 py-1 text-sm">
           create file "~/pages/index.vue" to replace this page
         </div>
         <div class="flex space-x-4 ml-2 mt-8 justify-center md:justify-start">
-          <AwesomeButton
-            size="lg"
-            :text="
-              parseMenuTitle(
-                awesome?.layout?.welcome?.primaryActionButton?.title ||
-                  'Nuxt 3',
-              )
-            "
-            :to="
-              parseMenuRoute(
-                awesome?.layout?.welcome?.primaryActionButton?.to ||
-                  'https://nuxt.com',
-              )
-            "
-            class="font-extrabold"
-          />
-          <AwesomeButton
-            v-if="
-              parseMenuRoute(
-                awesome?.layout?.welcome?.secondaryActionButton?.to ||
-                  awesome?.project?.links?.github,
-              )
-            "
-            :text="
-              parseMenuTitle(
-                awesome?.layout?.welcome?.secondaryActionButton?.title ||
-                  'Github',
-              )
-            "
-            :to="
-              parseMenuRoute(
-                awesome?.layout?.welcome?.secondaryActionButton?.to ||
-                  awesome?.project?.links?.github,
-              )
-            "
-            size="lg"
-            class="font-extrabold"
-            type="secondary"
-          />
+          <AwesomeButton size="lg" :text="parseMenuTitle(
+
+            'Nuxt 3',
+          )
+            " :to="parseMenuRoute(
+
+              'https://nuxt.com',
+            )
+              " class="font-extrabold" />
+          <AwesomeButton v-if="
+            parseMenuRoute(
+
+              awesome?.project?.links?.github,
+            )
+          " :text="parseMenuTitle(
+            'Github',
+          )
+            " :to="parseMenuRoute(
+              awesome?.project?.links?.github,
+            )
+              " size="lg" class="font-extrabold" type="secondary" />
         </div>
       </div>
       <div class="top-0 left-0 absolute w-screen">
         <div class="absolute right-0 top-0 w-1/4 h-screen py-10 pt-12 z-0 flex">
-          <div
-            class="flex-1 rounded-l-9xl bg-gradient-to-l from-blue-600/10"
-          ></div>
+          <div class="flex-1 rounded-l-9xl bg-gradient-to-l from-blue-600/10"></div>
         </div>
         <div class="absolute left-0 top-0 w-1/4 h-screen py-10 pt-12 z-0 flex">
-          <div
-            class="flex-1 rounded-r-9xl bg-gradient-to-r from-green-600/10"
-          ></div>
+          <div class="flex-1 rounded-r-9xl bg-gradient-to-r from-green-600/10"></div>
         </div>
       </div>
     </LayoutPageSection>
@@ -148,6 +117,7 @@ onMounted(() => {
 }
 
 @keyframes anim-fg-1 {
+
   0%,
   16.667%,
   100% {
@@ -161,6 +131,7 @@ onMounted(() => {
 }
 
 @keyframes anim-fg-2 {
+
   0%,
   16.667%,
   66.667%,
@@ -175,6 +146,7 @@ onMounted(() => {
 }
 
 @keyframes anim-fg-3 {
+
   0%,
   50%,
   100% {
@@ -226,11 +198,9 @@ onMounted(() => {
   -webkit-text-fill-color: transparent;
   padding-left: var(--padding);
   padding-right: var(--padding);
-  background-image: linear-gradient(
-    90deg,
-    var(--start-color),
-    var(--end-color)
-  );
+  background-image: linear-gradient(90deg,
+      var(--start-color),
+      var(--end-color));
   position: relative;
   opacity: 0;
   z-index: 1;
